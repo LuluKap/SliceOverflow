fbb73b07c105128763826149986419a1b961054a
AyubDel <ayubmainacc@gmail.com>
2025-10-27 14:44:09 -0400

    Update App.java
    added to app

--- src/main/java/pizza/shop/App.java
+++ src/main/java/pizza/shop/App.java
@@ -8,6 +8,8 @@ public class App {
         System.out.println("Connecting to PizzaShop DB...");
         try (Connection conn = DBConnection.getConnection()) {
             System.out.println("✅ Connected successfully to " + conn.getCatalog());
+            System.out.println("\nLaunching Customer Menu...");
+            CustomerEntry.main(args);
         } catch (Exception e) {
             e.printStackTrace();
         }
